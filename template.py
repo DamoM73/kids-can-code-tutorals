78670b0c4e04c93fa737d51e76b5f2408d5d05ef
DamoM73 <damomurtagh@gmail.com>
2018-12-21 15:24:52 +1000

    create tkinter template

---
+++ template.py
@@ -0,0 +1,14 @@
+from tkinter import *
+
+# DEFINE CONSTANTS
+# Screen dimentions
+WIDTH = 500                                     
+HEIGHT = 400
+
+# INITIALISE TKinter
+tk = Tk()                                           # create a tk object
+canvas = Canvas(tk, width=WIDTH, height=HEIGHT)     # create a canvas for the object
+tk.title("Drawing")                                 # change the window title
+canvas.pack()                                       # place the canvas in window
+
+canvas.mainloop()                                   # continue to run the canvas
