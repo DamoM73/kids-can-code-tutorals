80069b9b3ac10edb1fb13f34227821329b1d3cd2
DamoM73 <damomurtagh@gmail.com>
2018-12-21 15:45:51 +1000

    Lesson 2.2 completed

--- template.py
+++ template.py
@@ -8,7 +8,7 @@ HEIGHT = 400
 # INITIALISE TKinter
 tk = Tk()                                           # create a tk object
 canvas = Canvas(tk, width=WIDTH, height=HEIGHT)     # create a canvas for the object
-tk.title("Drawing")                                 # change the window title
+tk.title("Name")                                 # change the window title
 canvas.pack()                                       # place the canvas in window
 
 canvas.mainloop()                                   # continue to run the canvas
